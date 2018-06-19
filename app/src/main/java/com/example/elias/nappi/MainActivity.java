76297271769bd5401f7eb1e8bc76afa6bea23483
appeus <aaron.hyrkas@gmail.com>
2018-06-19 11:17:48 +0300

    Lisätty drawer_layout ja global_mode

--- app/src/main/java/com/example/elias/nappi/MainActivity.java
+++ app/src/main/java/com/example/elias/nappi/MainActivity.java
@@ -3,38 +3,41 @@ package com.example.elias.nappi;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Matrix;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Layout;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.Menu;
 import android.widget.AbsListView;
 import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
-
-    private float dX;
-    private float dY;
-
-    private ArrayList<Integer> buttons;
-    private int lastAction;
-    private ImageView nappi;
-
-
-    private Matrix matrix = new Matrix();
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+    Matrix matrix = new Matrix();
     Float scale = 1f;
     ScaleGestureDetector SGD;
 
-    private int mPtrCount;
+    private float dX;
+    private float dY;
+    private ArrayList<Integer> buttons;
+    private int lastAction;
+    private ImageView nappi;
+    private View.OnTouchListener onTouchListener;
+    private int Global_mode;
 
 
     @Override
@@ -42,47 +45,55 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Global_mode = 1;
         buttons = new ArrayList<>();
         nappi = (ImageView) findViewById(R.id.nappi);
+        //SGD = new ScaleGestureDetector(this, new ScaleListener());
 
-        SGD = new ScaleGestureDetector(this, new ScaleListener());
-        mPtrCount=0;
 
-        nappi.setOnTouchListener(this);
+        nappi.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                addbutton();
+                return false;
+            }
 
-    }
+        });
 
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:
-                mPtrCount++;
-                Log.d("mPtrCount: ",Integer.toString(mPtrCount));
-                if (mPtrCount==1) {
-                    addbutton();
-                    lastAction = MotionEvent.ACTION_DOWN;
-                    dX = view.getX() - motionEvent.getRawX();
-                    dY = view.getY() - motionEvent.getRawY();
+
+        onTouchListener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        lastAction = MotionEvent.ACTION_DOWN;
+                        dX = v.getX() - event.getRawX();
+                        dY = v.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        lastAction = MotionEvent.ACTION_MOVE;
+                        v.setX(event.getRawX() + dX);
+                        v.setY(event.getRawY() + dY);
+                        break;
                 }
-                if (mPtrCount ==2) {
-                    //starting distance between fingers
-                    lastAction = MotionEvent.ACTION_MOVE;
-                    view.setX(motionEvent.getRawX() + dX);
-                    view.setY(motionEvent.getRawY()  + dY);
+                return true;
+            }
+        };
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-                }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, null, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-                break;
-            case MotionEvent.ACTION_MOVE:
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-
-        }
-        return true;
     }
 
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-        public ScaleListener() {
-        }
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             scale = scale * detector.getScaleFactor();
@@ -95,26 +106,68 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     }
 
 
-
     public void addbutton() {
         Button b = new Button(this);
         int id = View.generateViewId();
         b.setText("nappi");
-        b.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+        b.setLayoutParams(new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT));
-        RelativeLayout rl = (RelativeLayout) findViewById(R.id.layout);
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.rllayout);
         rl.addView(b);
-        b.setOnTouchListener(this);
+        b.setOnTouchListener(onTouchListener);
         float Xpos = 800.0f;
         float Ypos = 800.0f;
         b.setX(Xpos);
         b.setY(Ypos);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_creator) {
+            Global_mode = 1;
+            Log.d("Global mode: ", "1");
+
+        } else if (id == R.id.nav_play) {
+            Global_mode = 2;
+            Log.d("Global mode: ", "2");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
 
 
+    }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void setLastAction(int lastAction) {
         this.lastAction = lastAction;
     }
 }
+
+
